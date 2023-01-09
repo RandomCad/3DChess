1c0da8eb6fa21afe4cc9f3ef7cd197ebbead9c2e
RandomCad <mmont4850@gmail.com>
2023-01-09 19:25:46 +0100

    add the ches board

--- AIDevelopment2/AIDevelopment2/Program.cs
+++ AIDevelopment2/AIDevelopment2/Program.cs
@@ -4,9 +4,10 @@ namespace AIDevelopment2
 {
     class Program
     {
+        int[,] ChesBoard = new int[8, 8];
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
